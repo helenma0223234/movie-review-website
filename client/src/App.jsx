8c0bc98ce65343618aadb3755627575049834f05
Yuchuan (Helen) Ma <69632858+helenma0223234@users.noreply.github.com>
2023-05-09 01:24:14 -0400

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
 
   return (
     <div className="App">
       <h2>Leave a review</h2>
       <div className="review-port">
         <label>Movie Name:</label>
-        <input type="text" name="movieName" />
+        <input type="text" name="movieName" onChange={(e) => { setMovieName(e.target.value); }} />
         <label>Movie review:</label>
-        <input type="text" name="review" />
+        <input type="text" name="review" onChange={(e) => { setReview(e.target.value); }} />
 
         <button type="submit">Submit</button>
       </div>
